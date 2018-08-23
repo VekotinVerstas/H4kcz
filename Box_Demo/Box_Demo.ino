734b24dd684f1c69d3b8502bdd51d3d324cad3ae
BaystoneJr <mikael.wiksten@outlook.com>
2018-08-23 14:20:14 +0300

    Servo and button setup

--- Box_Demo/Box_Demo.ino
+++ Box_Demo/Box_Demo.ino
@@ -1,11 +1,34 @@
+#include <Servo.h>
 
+Servo boxservo;
+#define BUTTONPIN D3
 
-void setup() {
+void setup () {
+  boxservo.attach(D2);
+  pinMode(BUTTONPIN, INPUT_PULLUP);
   
+}
+
+void loop () {
+  int pos;
+  int value = digitalRead(BUTTONPIN);
+
+  pos = 0;
+  boxservo.write(pos);
+  delay(10000);
+  
+
+ if(value == LOW) {
+  for (pos = 0; pos <= 180; pos += 1){
+    boxservo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0;pos -= 1) {
+    boxservo.write(pos);
+    delay(15);
+  }
+  
+ }
 
 }
 
-void loop() {
-  
-
-}

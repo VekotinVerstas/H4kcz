4ecf559642aea4a0d457fd706183bae2e091be3b
BaystoneJr <mikael.wiksten@outlook.com>
2018-08-24 13:45:51 +0300

    Add WiFi connection

--- Box_Demo/Box_Demo.ino
+++ Box_Demo/Box_Demo.ino
@@ -1,28 +1,49 @@
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+
+#include <Bounce2.h>
 #include <Servo.h>
+#include "secrets.h" //Create your own secrets.h from the example
 
 Servo boxservo;
-#define BUTTONPIN D3
 
-void setup () {
-  boxservo.attach(D2);
-  pinMode(BUTTONPIN, INPUT_PULLUP);
-  
+int pos;
+
+int openendCounter;
+
+void sendOpenedInfo () {
+    
 }
 
-void loop () {
-  int pos;
-  int value = digitalRead(BUTTONPIN);
+void setup () {
+  Serial.begin(115200);
+  boxservo.attach(D2);
+  pinMode(D3, INPUT_PULLUP);
+  WiFi.begin(WIFI_SSID, WIFI_PSWD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting....");
+  }
 
   pos = 0;
   boxservo.write(pos);
-  delay(10000);
+
+}
+
+void loop () {
   
+  int value = digitalRead(D3);
+
+
 
  if(value == LOW) {
+  Serial.println("Nappi painettu");
   for (pos = 0; pos <= 180; pos += 1){
     boxservo.write(pos);
     delay(15);
   }
+  
   for (pos = 180; pos >= 0;pos -= 1) {
     boxservo.write(pos);
     delay(15);
